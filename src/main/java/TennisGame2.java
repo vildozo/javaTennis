8955879628d9f50388161cabdfe362d13a508f5d
vildozo <vildozo@outlook.com>
2017-02-19 10:37:39 -0400

    eliminar codigo innecesario

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -6,12 +6,8 @@ public class TennisGame2 implements TennisGame
     
     public String P1res = "";
     public String P2res = "";
-    private String player1Name;
-    private String player2Name;
 
     public TennisGame2(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
     }
 
     public String getScore(){
